a7ec07e8b8f04a3c1642e70936aa478e423f146d
Md. Dulal Prodhan <96787662+prodhan482@users.noreply.github.com>
2025-11-27 18:53:36 +0600

    updated

--- resources/views/include/web/header.blade.php
+++ resources/views/include/web/header.blade.php
@@ -1,7 +1,3 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-
-
 <style>
     .navbar-brand span {
         color: #002C4D !important;
@@ -20,24 +16,28 @@
     }
 </style>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 
 
 <nav class="navbar navbar-expand-lg bg-light shadow-lg">
     <div class="container">
         <a class="navbar-brand" href="/">
             <img src="assets/images/logo.png" class="logo img-fluid" alt="ClearmenGWR">
-            <span>
-                Clear Men Guinness World Records
-            </span>
-
+            <span>Clear Men Guinness World Records</span>
             <p style="color: #002C4D; font-size: 4px; margin: 5px 0 0 0;">
-                <b>PEOPLE IN AN ONLINE VIDEO CHAIN<br> PASSING A FOOTBALL (SOCCER BALL)</b>
+                <b>PEOPLE IN AN ONLINE VIDEO CHAIN<br>PASSING A FOOTBALL (SOCCER BALL)</b>
             </p>
         </a>
 
+        <!-- Navbar Toggler -->
+        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-
                 @guest
                     <li class="nav-item">
                         @if(Request::is('/'))
@@ -69,7 +69,6 @@
                             <a class="nav-link" href="{{ '/user-dashboard' }}">Dashboard</a>
                         </li>
                     @endif
-
                     <li class="nav-item">
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
@@ -81,3 +80,6 @@
         </div>
     </div>
 </nav>
+
+<!-- Bootstrap JS (required for toggle) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
